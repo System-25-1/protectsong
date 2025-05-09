3c93c2f9e44f4899fc2848aafa3b722234fe98d4
eunseo.yoo <yooes0220@sookmyung.ac.kr>
2025-05-09 15:18:16 +0900

    메인 1차 수정

---
+++ app/src/main/java/com/example/protectsong/EditProfileActivity.kt
@@ -0,0 +1,12 @@
+// EditProfileActivity.kt
+package com.example.protectsong
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EditProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile) // XML은 따로 만들어야 함
+    }
+}
