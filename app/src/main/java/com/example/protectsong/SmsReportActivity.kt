61b759675c565ce136ed742292fef25e60f5c420
jaeee25 <limjaeseo@sookmyung.ac.kr>
2025-05-14 16:06:18 +0900

    로그인,회원가입 변경

--- app/src/main/java/com/example/protectsong/SmsReportActivity.kt
+++ app/src/main/java/com/example/protectsong/SmsReportActivity.kt
@@ -1,9 +1,12 @@
 package com.example.protectsong
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.example.protectsong.databinding.ActivitySmsReportBinding
+import com.example.protectsong.MainActivity
+
 
 class SmsReportActivity : AppCompatActivity() {
 
@@ -13,6 +16,14 @@ class SmsReportActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySmsReportBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //뒤로가면 mainactivity
+        binding.backButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish()
+        }
 
         // 신고 유형 스피너 설정
         val typeAdapter = ArrayAdapter.createFromResource(
@@ -32,5 +43,22 @@ class SmsReportActivity : AppCompatActivity() {
         )
         buildingAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         binding.spinnerBuilding.adapter = buildingAdapter
+
+        binding.btnSubmit.setOnClickListener {
+            // 팝업 다이얼로그 생성
+            val builder = android.app.AlertDialog.Builder(this)
+            builder.setTitle("신고 완료!")
+            builder.setMessage("신고가 성공적으로 접수되었습니다.")
+            builder.setPositiveButton("확인") { dialog, _ ->
+                dialog.dismiss()
+                // MainActivity로 이동
+                val intent = Intent(this, MainActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                finish() // 현재 액티비티 종료
+            }
+            builder.show()
+        }
+
     }
 }
