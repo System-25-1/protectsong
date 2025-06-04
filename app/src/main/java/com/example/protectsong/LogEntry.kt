62001720b266bf1263a9688bff1cf0c639e23b00
eunseo.yoo <yooes0220@sookmyung.ac.kr>
2025-06-04 14:25:23 +0900

    log csv 구현

--- app/src/main/java/com/example/protectsong/LogEntry.kt
+++ app/src/main/java/com/example/protectsong/LogEntry.kt
@@ -2,6 +2,7 @@ package com.example.protectsong.model
 
 data class LogEntry(
     val userId: String = "",
+    val studentId: String = "",
     val action: String = "",
     val detail: String = "",
     val timestamp: Long = 0L
